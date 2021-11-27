d92d31f2154ef61b8f14d8e11c8296848bd87280
corb1999 <le.corbusier.29845@gmail.com>
2021-11-26 20:35:52 -0500

    just tiny changes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 personal-use web crawling project/practice
 
 notes:
-- utilize the rvest tidyverse package to crawl and compile data
-- structure = crawler runs and reads the html, then outputs a csv of the parsed data to a csv file in the ore folder, then create a sqlite db to house the crawled results; 
+- utilize the rvest tidyverse package to crawl and compile data from a website
+- structure = crawler runs and reads the html, then outputs a csv of the parsed data to a csv in the ore folder, then create a sqlite db to house the crawled results, pre-process the data in the database with a view, then query that view and analyze and visualize in an r script; 
 
 Confirming that it is ok to review the shop portion of the site by confirming it is not in the robots.txt file disallows:
 https://www.evo.com/robots.txt
--- etl/db_sqlite/create_basic_view.sql
+++ etl/db_sqlite/create_basic_view.sql
@@ -10,7 +10,7 @@ replace(product, '<U+200B>', '') AS product,
 length(replace(product, '<U+200B>', '')) - 
     length(replace(replace(product, '<U+200B>', ''), 
     '+', '')) + 1 AS products_in_package, 
-regular_price, 
+--regular_price, 
 IIF(instr(regular_price, ' - ') > 0, 
     TRUE, FALSE) AS has_price_range_ind, 
 cast(replace(replace(IIF(IIF(instr(regular_price, ' - ') > 0, 
@@ -24,7 +24,7 @@ cast(replace(replace(IIF(IIF(instr(regular_price, ' - ') > 0,
                 length(regular_price)), 
         regular_price), '$', ''), 
         ',', '') AS NUMERIC) AS regular_price_val_toprange, 
-discount_price, 
+--discount_price, 
 cast(replace(replace(IIF(IIF(instr(
     replace(replace(replace(discount_price, ' Limited Time', ''), ' Sale', ''), 'Outlet: ', ''), 
         ' - ') > 0, TRUE, FALSE) = 1, 
@@ -35,11 +35,11 @@ cast(replace(replace(IIF(IIF(instr(
             ' - ')), 
         replace(replace(replace(discount_price, ' Limited Time', ''), ' Sale', ''), 'Outlet: ', '')), 
             '$', ''), ',', '') AS NUMERIC) AS discount_price_val, 
-url_page, 
+--url_page, 
 cast(replace(url_page, 'p_', '') AS INT) AS url_page_num, 
 datetime(crawled_runtime) AS crawled_runtime, 
 strftime('%Y %m %d', crawled_runtime) AS crawled_date, 
-crawled_url
+--crawled_url
 FROM tbl_alpha;
 
 /* 
