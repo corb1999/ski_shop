4e89bc14dc7f8e977666adfa61516ae3fee3089b
corb1999 <le.corbusier.29845@gmail.com>
2021-11-26 20:12:10 -0500

    addresses issue #3. also includes some improvements to the sqlite view

---
+++ eda/basic_eda.R
@@ -0,0 +1,125 @@
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# INTRO ====================================================================
+metadatar <- list(script_starttime = Sys.time(), 
+                  script_det = list(version_dt = as.Date("2021-11-26"), 
+                                    author = "corb", 
+                                    proj_name = "ski_shop", 
+                                    script_type = "eda", 
+                                    notepad = paste0("basic query explore")), 
+                  seed_set = 6)
+metadatar
+
+# LOAD LIBRARIES **********************************************************
+R.version.string
+Sys.info()
+getwd()
+library(lobstr)
+library(rlang)
+library(tidyverse)
+library(tidylog)
+library(lubridate)
+library(scales)
+library(gt)
+library(DBI)
+library(RSQLite)
+set.seed(metadatar$seed_set[1])
+options(digits = 4, max.print = 99, warnPartialMatchDollar = TRUE, 
+        tibble.print_max = 30, scipen = 999, nwarnings = 5)
+mem_used()
+
+# basic helper functions ***************************************************
+
+# function to print object size
+sizer <- function(x) {
+  aaa <- format(object.size(x), "MB")
+  return(aaa)}
+
+# function to quickly run garbage collection
+trash <- function(x) {
+  gc(verbose = TRUE)}
+
+# function to quickly view a sample of a dataframe
+viewer <- function(x) {
+  if (is.data.frame(x) == FALSE) {
+    print("Error, insert a dataframe")
+  } else {
+    if(nrow(x) < 95) {
+      View(x[sample(1:nrow(x), floor(nrow(x) * 0.5)), ])
+    } else {
+      View(x[sample(1:nrow(x), 100), ])
+    }}}
+
+# a function to make a quick data dictionary of a data frame
+data_dictionary <- function(aa) {
+  dd <- data.frame(column_order = seq(1, ncol(aa)), 
+                   column_name_text = colnames(aa), 
+                   column_class = sapply(aa, class, simplify = TRUE), 
+                   column_nacount = sapply(lapply(aa, is.na), 
+                                           sum, simplify = TRUE), 
+                   column_uniques = sapply(lapply(aa, unique), 
+                                           length, simplify = TRUE), 
+                   row_01 = sapply(aa[1, ], as.character, simplify = TRUE), 
+                   row_02 = sapply(aa[2, ], as.character, simplify = TRUE),
+                   row_03 = sapply(aa[3, ], as.character, simplify = TRUE),
+                   row_04 = sapply(aa[4, ], as.character, simplify = TRUE),
+                   row_05 = sapply(aa[5, ], as.character, simplify = TRUE),
+                   row.names = NULL)
+  return(dd)}
+
+# start the clock timer, used for monitoring runtimes
+clockin <- function() {
+  aa <- Sys.time()
+  clock_timer_start <<- aa
+  return(aa)}
+
+# end the clock timer, used in conjunction with the clockin fun
+clockout <- function(x) {
+  aa <- clock_timer_start
+  bb <- Sys.time()
+  cc <- bb - aa
+  return(cc)}
+
+# helps turn a character dollar variable into numeric
+#   requires stringr, uncomment last line to turn NA to zero
+cash_money <- function(x) {
+  aa <- str_remove_all(x, pattern = "\\$")
+  bb <- str_remove_all(aa, pattern = ",")
+  cc <- as.numeric(bb)
+  # cc <- ifelse(is.na(cc), 0, cc)
+  return(cc)}
+
+# POST SCRIPT; alt to using paste0() all the time (i saw this on twitter)
+'%ps%' <- function(lhs, rhs) {
+  return_me <- paste0(lhs, rhs)
+  return(return_me)}
+
+# ^ ====================================
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# query the sqlite database --------------------------------------
+
+connection_db <- dbConnect(SQLite(), 
+                           dbname = paste0(getwd(), 
+                                           '/etl/db_sqlite', 
+                                           '/ski_shop_db.db'))
+dbListTables(connection_db)
+
+sql_to_run <- dbSendQuery(connection_db, 'SELECT * FROM view_gamma;')
+df <- dbFetch(sql_to_run)
+df <- df %>% as_tibble()
+dim(df)
+df
+
+# ^ -----
+
+# basic viz ------------------------------------------
+
+df %>% filter(crawled_date == '2021 11 26') %>% 
+  ggplot(aes(x = regular_price_val)) + 
+  geom_histogram(bins = 30, fill = 'blue', 
+                 color = 'white', alpha = 0.5) + 
+  theme_minimal()
+
+
+
+# ^ -----
